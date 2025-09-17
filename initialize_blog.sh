db8b5246a1a6ac1cce33ddf425c605fa190e97c0
Qihang Zhang <qihangz.work@gmail.com>
2025-09-17 09:36:57 -0700

    auto publish

--- initialize_blog.sh
+++ initialize_blog.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-REPO_URL="git@github.com:Qihang-Zhang/customized_mkdocs.git"
+set -euo pipefail
+
+REPO_URL="git@github.com:Qihang-Zhang/Comping.git"
 TARGET_DIR="./customized_mkdocs"
 
 # Remove existing directory if present
@@ -10,8 +12,5 @@ fi
 # Fresh clone
 git clone "$REPO_URL" "$TARGET_DIR"
 
-# Source aliases and copy config
-chmod +x "$TARGET_DIR"
-source "$TARGET_DIR/mkdocs_alias.sh"
+zsh ./customized_mkdocs/maintain_config/copy_config.sh
 
-$TARGET_DIR/maintain_config/copy_config.sh
